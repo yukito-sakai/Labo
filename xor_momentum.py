b96d592bc2771f10b41c52f95345b04c512a000b
yukito-saskai <71584056+yukito-sakai@users.noreply.github.com>
2021-05-20 15:25:44 +0900

    1

--- xor_momentum.py
+++ xor_momentum.py
@@ -6,6 +6,7 @@ def sig(s):
 x = np.array([[1, 0, 0],[1, 0, 1], [1, 1, 0], [1, 1, 1]])
 t = np.array([[0, 1],[1, 0], [1, 0], [0, 1]])
 
+np.random.seed(0)
 m = 5
 w1 = np.random.rand(m, 3)   
 w2 = np.random.rand(2, m+1)
@@ -14,7 +15,7 @@ dw2 = np.zeros(w2.shape)
 
 rho = 0.3
 mu = 0.9
-v = 
+v = 0
 
 while True:
 
@@ -27,6 +28,10 @@ while True:
 
     dw2 = ((o2 - t.T) * (1 - o2) * o2).dot(o1.T)
     dw1 = ((((o2 - t.T) * (1 - o2) * o2).T).dot(w2).T * ((1 - o1) * o1)).dot(x)[1:]
+    
+    v2 = mu * v2 - rho * dw2 # integrate velocity
+    x2 += v2
+    
     w2 -= rho * dw2
     #print(dw1)
     w1 -= rho * dw1
