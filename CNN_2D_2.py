a814cf46f74811bd6906933607002e26d9df9746
yukito-sakai <71584056+yukito-sakai@users.noreply.github.com>
2021-06-22 17:00:24 +0900

    2

---
+++ CNN_2D_2.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read_data (type:dict)
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:           #rb = read binary(2進法)、with関数を使うことでopenするだけでなく、closeも勝手にやってくれる
+        dict = pickle.load(fo, encoding='bytes')   #文字形式をbytesで読み込む
+    return dict
+
+# load data from file data_batch_1
+d = unpickle('./cifar-10-batches-py/data_batch_1')
+
+# d is a dict. b'data' key contains the image data
+firstimage = d[b'data'][0]
+
+# Plotting the image data
+# The image data is 3 x 32 x 32 = 3072 values stored in one-dimensional array,
+# first 32 x 32 values for red, followed by 32 x 32 values for green, and then
+# blue.
+# So we first convert it to a 3 dimensional array using np.reshape.
+# But to plot the image, we need the red, green, blue values for each pixel
+# together, in the shape 32 x 32 x 3, so we np.transpose the data to change the
+# order of values. The axe
+# s parameter describes how to reorder the data.
+plt.imshow(np.transpose(np.reshape(firstimage, (3, 32, 32)), axes=(1, 2, 0)))
+plt.show()
+
+
+
+# Conv
+def Conv(x):
+    #x = np.random.randint(-3, 3, (4,4))
+    w = np.random.randint(-1, 1, (3,3))
+    out = []
+
+    pad = 1
+    stride = 1
+
+    for k in range(0, len(x)+2*pad -len(w)+1, stride):
+        out_row = []
+        for i in range(0, len(x)+2*pad -len(w)+1, stride):
+            z = 0
+            for l in range(len(w)):
+                for j in range(len(w)):
+                    if((0 <= k+l-pad) and (k+l-pad < len(x))):
+                        if((0 <= i+j-pad) and (i+j-pad < len(x))):
+                            z += x[k+l-pad][i+j-pad] * w[l][j]
+            out_row.append(z)
+        out.append(out_row)  
+        
+    out = np.array(out)
+    #print('conv')
+    print(out)
+    return(out)
+
+#ReLu
+def ReLu(x):
+    return np.where(x > 0, x, 0)
+
+# MaxPooling
+def MaxPooling(out):
+    out_2 = []
+    out_2 = np.maximum(np.maximum(out[0::2,0::2], out[0::2,1::2]),np.maximum(out[1::2,0::2], out[1::2,1::2]))  # 0::2 0番目の値から２の倍数をとってくる
+    print(out_2)
+    return out_2
+
+#Fully Connected
+def FC(out_2):
+    out_2 = np.ravel(out_2)       #１次元配列にする
+    weight = np.random.rand(2,len(out_2))
+    bias = np.random.rand(2)
+
+    fc = weight.dot(out_2) + bias
+    fc = np.array(fc)
+    print(fc)
+    return fc
+    
+# Softmax
+def softmax(x):
+    m = np.max(x)  # To avoid the overflow of exp(x)
+    return np.exp(x-m)/np.sum(np.exp(x-m))
+
+
+np.random.seed(0)
+
+u = np.random.randint(-3, 3, (4,4))
+v = Conv(u)
+w = ReLu(v)
+x = MaxPooling(w)
+y = FC(x)
+z = softmax(y)
+print(z)
--- nn.py
+++ nn.py
@@ -1,48 +1,65 @@
-from sys import version
-from typing import ValuesView
-from numpy.lib.function_base import kaiser
-from numpy.testing._private.nosetester import _numpy_tester
+import numpy as np
+import matplotlib.pyplot as plt
 
+# read_data (type:dict)
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
-import random
+# load data from file data_batch_1
+d = unpickle('./cifar-10-batches-py/data_batch_1')
+
+# d is a dict. b'data' key contains the image data
+firstimage = d[b'data'][0]
+
+# Plotting the image data
+# The image data is 3 x 32 x 32 = 3072 values stored in one-dimensional array,
+# first 32 x 32 values for red, followed by 32 x 32 values for green, and then
+# blue.
+# So we first convert it to a 3 dimensional array using np.reshape.
+# But to plot the image, we need the red, green, blue values for each pixel
+# together, in the shape 32 x 32 x 3, so we np.transpose the data to change the
+# order of values. The axes parameter describes how to reorder the data.
+plt.imshow(np.transpose(np.reshape(firstimage, (3, 32, 32)), axes=(1, 2, 0)))
+plt.show()
+
 
 import numpy as np
-from scipy.misc import imread, imsave, imresize
-img = imread('assets/cat.jpg')
-print(img.dtype, img.shape) 
-mg_tinted = img * [1, 0.95, 0.9]
-img_tinted = imresize(img_tinted, (4, 4))
-imsave('assets/cat_tinted.jpg', img_tinted)
 
-def softmax(s):
-    max = np.amax(np.array(s))
-    return (np.exp(s) / max) / (np.sum(np.exp(s)) / max)
+# Conv
+x = np.array([1, 2, 3, 4, 5])
+w = np.array([1, 1, 1])
+out = []
 
+pad = 0
+stride = 1
 
-#CONV_LAYER
-#4*4*1
-w1 = 4
-h1 = 4
-d1 = 1
-weight = 10
-bias = 1
+for i in range(0, len(x)+2*pad -len(w)+1, stride):
+    z = 0
+    for j in range(len(w)):
+        if((0 <= i+j-pad) and (i+j-pad < len(x))):
+            z += x[i+j-pad] * w[j]
+    out.append(z)
+print(out)
 
-k = 3
-f = 3
-s = random.randrange(1, 3) #stride
-p = random.randrange(3) #padding
-w2 = (w1 - f - 2*p) / s + 1
-h2 = (h1 - f - 2*p) / s + 1
-d2 = k
+# MaxPooling
+x = np.array(list(range(10)))  # x=[0,1,2,3,4,5,6,7,8,9]
+out = []
 
-for k in range (d1):
-    for j in range(h1 + p):
-        for i in range(w1 + p):
-            v[i,j,k] =  np.sum(x[i*s:f+i*s,j*s:f+j*s,:] * weight) + bias
-            ｚ
-            
+#for i in range(0, len(x), 2):
+#   out.append(np.max(x[i:][:2]))  # x[i:i+2] is OK
+#=print(out)
 
+out = np.maximum(x[0::2], x[1::2])  # 0::2 0番目の値から２の倍数をとってくる
+print(out)
 
+# Softmax
+def softmax(x):
+    m = np.max(x)  # To avoid the overflow of exp(x)
+    return np.exp(x-m)/np.sum(np.exp(x-m))
 
-
-
+x = np.array([1, 2, 3])
+y = softmax(x)
+print(y)
